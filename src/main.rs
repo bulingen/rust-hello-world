1956eceb0de9987dad99e04930e9cd4d69541371
bulingen <adamlecorney@gmail.com>
2024-05-16 01:01:00 +0200

    feat: simple guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use rand::Rng;
+use std::io::{self, Write};
+
+fn get_user_input() -> u32 {
+    // Print a prompt for the user
+    print!("Please enter a number: ");
+    // Ensure the prompt is displayed immediately
+    io::stdout().flush().unwrap();
+
+    // Create a mutable String to store the user input
+    let mut input = String::new();
+
+    // Read the input from the standard input (stdin)
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // Trim the input to remove any extra whitespace and convert it to an integer
+    let number: u32 = input.trim().parse().expect("Please type a number!");
+    println!("You entered: {}", number);
+    number
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let random_number: u32 = rng.gen_range(1..101);
+    // println!("{}", random_number);
+
+    // // Print a prompt for the user
+    // print!("Please enter a number: ");
+    // // Ensure the prompt is displayed immediately
+    // io::stdout().flush().unwrap();
+
+    // // Create a mutable String to store the user input
+    // let mut input = String::new();
+
+    // // Read the input from the standard input (stdin)
+    // io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    // // Trim the input to remove any extra whitespace and convert it to an integer
+    // let number: i32 = input.trim().parse().expect("Please type a number!");
+    // for _x in 0..5 {
+    //     let guess = get_user_input();
+    //     println!("{}", guess);
+    // }
+
+    let mut found_it = false;
+
+    while !found_it {
+        let guess = get_user_input();
+        if guess == random_number {
+            found_it = true;
+            println!("Yes, the number was {}", random_number);
+        } else if guess < random_number {
+            println!("Higher!");
+        } else if guess > random_number {
+            println!("Lower!");
+        }
+    }
 }
