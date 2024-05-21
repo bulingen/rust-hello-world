f31ecb11f0385ce4fef6c4a41a39d67cb994cbee
bulingen <adamlecorney@gmail.com>
2024-05-21 14:31:17 +0200

    refactor: use match instead of if:s

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use rand::Rng;
-use std::io::{self, Write};
+use std::{
+    cmp::Ordering,
+    io::{self, Write},
+};
 
 fn get_user_input() -> u32 {
     // Print a prompt for the user
@@ -24,37 +27,22 @@ fn get_user_input() -> u32 {
 fn main() {
     let mut rng = rand::thread_rng();
     let random_number: u32 = rng.gen_range(1..101);
-    // println!("{}", random_number);
-
-    // // Print a prompt for the user
-    // print!("Please enter a number: ");
-    // // Ensure the prompt is displayed immediately
-    // io::stdout().flush().unwrap();
-
-    // // Create a mutable String to store the user input
-    // let mut input = String::new();
-
-    // // Read the input from the standard input (stdin)
-    // io::stdin().read_line(&mut input).expect("Failed to read line");
-
-    // // Trim the input to remove any extra whitespace and convert it to an integer
-    // let number: i32 = input.trim().parse().expect("Please type a number!");
-    // for _x in 0..5 {
-    //     let guess = get_user_input();
-    //     println!("{}", guess);
-    // }
 
     let mut found_it = false;
 
     while !found_it {
         let guess = get_user_input();
-        if guess == random_number {
-            found_it = true;
-            println!("Yes, the number was {}", random_number);
-        } else if guess < random_number {
-            println!("Higher!");
-        } else if guess > random_number {
-            println!("Lower!");
+        match guess.cmp(&random_number) {
+            Ordering::Equal => {
+                found_it = true;
+                println!("Yes, the number was {}", random_number);
+            }
+            Ordering::Less => {
+                println!("Higher!");
+            }
+            Ordering::Greater => {
+                println!("Lower!");
+            }
         }
     }
 }
